b0c1714668fb2f20b2c1b5227a088a3b15432bae
Aaron Diebold <diebolav@gmail.com>
2024-07-20 12:45:37 -0400

    Changed parameter calculation to single method.

--- tl_metasurface/antenna.py
+++ tl_metasurface/antenna.py
@@ -8,7 +8,7 @@ Z_0 = np.sqrt(MU_0/EPS_0)
 
 class Antenna():
 
-    def __init__(self, f, element, Lx, delta_x):
+    def __init__(self, element, f, Lx, delta_x):
         
         self.params = {}
         self.element = element
@@ -17,8 +17,13 @@ class Antenna():
         self.params['Lx'] = Lx
         self.params['delta_x'] = delta_x
 
+        self.calculate_parameters()
+
     def compute_fields(self, tuning_state):
         print('TBD')
+        return
+    
+    def calculate_parameters(self):
         return
     
     def __str__(self):
@@ -31,10 +36,14 @@ class Antenna():
     
 class RectangularWaveguide(Antenna):
     
-    def __init__(self, f, element, Lx, delta_x, a, b, **kwargs):
+    def __init__(self, element, f, Lx, delta_x, a, b, **kwargs):
         
-        super().__init__(f, element, Lx, delta_x)
+        self.params = {}
+        self.element = element
         
+        self.params['f'] = f
+        self.params['Lx'] = Lx
+        self.params['delta_x'] = delta_x
         self.params['a'] = a
         self.params['b'] = b
         self.params['m'] = kwargs.get('m', 1)
@@ -42,50 +51,28 @@ class RectangularWaveguide(Antenna):
         self.params['epsilon_r'] = kwargs.get('epsilon_r', 1)
         self.params['tan_delta'] = kwargs.get('tan_delta', 0)
         self.params['sigma_wall'] = kwargs.get('sigma_wall', 0)
+
+        self.calculate_parameters()
+
+    def calculate_parameters(self):
+
         self.params['omega'] = 2*np.pi * self.params['f']
         self.params['k'] = self.params['omega'] * np.sqrt(MU_0*EPS_0*self.params['epsilon_r'])
         self.params['lambda'] = 2*np.pi / self.params['k']
-        self.params['beta_g'] = self.beta_g()
+        self.params['beta_g'] = np.sqrt(self.params['k']**2 - (np.pi*self.params['m']/self.params['a'])**2 - (np.pi*self.params['n']/self.params['b'])**2)
         self.params['lambda_g'] = 2*np.pi / self.params['beta_g']
-        self.params['f_c'] = self.cutoff_frequency()
-        self.params['R_s'] = self.surface_resistance()
-        self.params['alpha_d'] = self.alpha_d()
-        self.params['alpha_c'] = self.alpha_c()
+        self.params['f_c'] = 1/(2 * np.sqrt(MU_0*EPS_0*self.params['epsilon_r'])) * np.sqrt((self.params['m']/self.params['a'])**2 + (self.params['n']/self.params['b'])**2)
+        self.params['R_s'] = np.sqrt(self.params['omega']*MU_0 / (2*self.params['sigma_wall']))
+        self.params['alpha_d'] = self.params['k']**2 * self.params['tan_delta'] / (2 * self.params['beta_g'])
+        self.params['alpha_c'] = ( (self.params['R_s'] / (self.params['a']**3 * self.params['b'] * self.params['beta_g'] * self.params['k'] * Z_0))
+                                    * (2 * self.params['b'] * np.pi**2 + self.params['a']**3 * self.params['k']**2) )
         self.params['alpha'] = self.params['alpha_d'] + self.params['alpha_c']
-
-    def beta_g(self):
-        '''
-        Calculate the propagation constant for TE_mn mode.
-        '''
-        return np.sqrt(self.params['k']**2 - (np.pi*self.params['m']/self.params['a'])**2 - (np.pi*self.params['n']/self.params['b'])**2)
-    
-    def cutoff_frequency(self):
-        '''
-        Calculate the cutoff frequency for TE_mn mode.
-        '''
-        return 1/(2 * np.sqrt(MU_0*EPS_0*self.params['epsilon_r'])) * np.sqrt((self.params['m']/self.params['a'])**2 + (self.params['n']/self.params['b'])**2)
-    
-    def alpha_d(self):
-        '''
-        Calculate the dielectric attenuation constant.
-        '''
-        return self.params['k']**2 * self.params['tan_delta'] / (2 * self.params['beta_g'])
-    
-    def surface_resistance(self):
-        '''
-        Calculate the surface resistance.
-        '''
-        return np.sqrt(self.params['omega']*MU_0 / (2*self.params['sigma_wall']))
-    
-    def alpha_c(self):
-        '''
-        Calculate the conductor attenuation constant.
-        '''
-        return ((self.params['R_s'] / (self.params['a']**3 * self.params['b'] * self.params['beta_g'] * self.params['k'] * Z_0))
-                * (2 * self.params['b'] * np.pi**2 + self.params['a']**3 * self.params['k']**2))
 
 class SIW(RectangularWaveguide):
 
-    def __init__(self, f, element, Lx, delta_x, a, b, via_pitch, via_diameter, **kwargs):
+    def __init__(self, element, f, Lx, delta_x, a, b, via_pitch, via_diameter, **kwargs):
 
-        super().__init__(f, element, Lx, delta_x, a, b, **kwargs)
+        a = a - 1.08 * (via_diameter**2)/via_pitch + 0.1 * (via_diameter**2)/a
+        super().__init__(element, f, Lx, delta_x, a, b, **kwargs)
+        self.params['via_pitch'] = via_pitch
+        self.params['via_diameter'] = via_diameter
