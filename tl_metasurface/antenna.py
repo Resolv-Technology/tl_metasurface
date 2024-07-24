21d5938b4cc77d1027f5f075cb7cdc818f6240b4
Aaron Diebold <diebolav@gmail.com>
2024-07-23 22:39:48 -0400

    Added feed options.

--- tl_metasurface/antenna.py
+++ tl_metasurface/antenna.py
@@ -48,6 +48,38 @@ class Antenna():
         S21 = 2 / denominator
         S22 = (-A[:,0,0] + A[:,0,1]/self.params['Z'] - A[:,1,0]*self.params['Z'] + A[:,1,1]) / denominator
         return np.transpose(np.array([[S11, S12], [S21, S22]]), (2, 0, 1))
+    
+    def cascade(self, indices, tuning_state):
+        Ai = np.empty((indices.size, 2, 2), dtype=complex)
+        Sn_plus = np.empty((indices.size, 2, 2), dtype=complex)
+        Sn_minus = np.empty((indices.size, 2, 2), dtype=complex)
+        for i in range(indices.size):
+            Ai[i,:,:] = self.A0 @ self.element.A[tuning_state[indices[i]],:,:]   # make Ai_tilde for each element depending on tuning state
+
+        for n in range(indices.size):
+            if n == 0:
+                An = self.A0
+            else:
+                An = np.eye(2)
+                for i in range(n-1):
+                    An = An @ Ai[i,:,:]
+                An = An @ self.A0
+            Sn_plus[n,:,:] = self.make_S(An[None,:,:])[0,:,:]
+
+            An = np.eye(2)
+            for i in range(n, indices.size):
+                An = An @ Ai[i,:,:]
+            Sn_minus[n,:,:] = self.make_S(An[None,:,:])[0,:,:]
+
+        V_plus = Sn_plus[:,1,0]
+        V_minus = V_plus * Sn_minus[:,0,0]
+        C_10 = V_plus + V_minus
+        Ey = -1j*self.params['omega'] * MU_0 * np.pi / (self.params['k_c']**2 * self.params['a']) * np.sin(np.pi*self.element.x[indices]/ self.params['a']) * C_10
+        Hx = 1j*self.params['beta_g'] * np.pi / (self.params['k_c']**2 * self.params['a']) * np.sin(np.pi*self.element.x[indices]/ self.params['a']) * C_10
+        Hz = np.cos(np.pi*self.element.x[indices]/ self.params['a']) * C_10
+        E = np.stack((np.zeros_like(Ey), Ey, np.zeros_like(Ey)), axis=1)
+        H = np.stack((Hx, np.zeros_like(Hx), Hz), axis=1)
+        return E, H
 
     def compute_fields(self, tuning_state, feed_position='left'):
         '''
@@ -58,35 +90,29 @@ class Antenna():
             feed_position: position of feed ('left', 'right', 'both', or 'center'. default: 'left')
         '''
         if feed_position == 'left':
-            Ai = np.empty((self.params['N'], 2, 2), dtype=complex)
-            Sn_plus = np.empty((self.params['N'], 2, 2), dtype=complex)
-            Sn_minus = np.empty((self.params['N'], 2, 2), dtype=complex)
-            for i in range(self.params['N']):
-                Ai[i,:,:] = self.A0 @ self.element.A[tuning_state[i],:,:]   # make Ai_tilde for each element depending on tuning state
-
-            for n in range(self.params['N']):
-                if n == 0:
-                    An = self.A0
-                else:
-                    An = np.eye(2)
-                    for i in range(n-1):
-                        An = An @ Ai[i,:,:]
-                    An = An @ self.A0
-                Sn_plus[n,:,:] = self.make_S(An[None,:,:])[0,:,:]
-
-                An = np.eye(2)
-                for i in range(n, self.params['N']):
-                    An = An @ Ai[i,:,:]
-                Sn_minus[n,:,:] = self.make_S(An[None,:,:])[0,:,:]
-
-            V_plus = Sn_plus[:,1,0]
-            V_minus = V_plus * Sn_minus[:,0,0]
-            C_10 = V_plus + V_minus
-            Ey = -1j*self.params['omega'] * MU_0 * np.pi / (self.params['k_c']**2 * self.params['a']) * np.sin(np.pi*self.element.x/ self.params['a']) * C_10
-            Hx = 1j*self.params['beta_g'] * np.pi / (self.params['k_c']**2 * self.params['a']) * np.sin(np.pi*self.element.x/ self.params['a']) * C_10
-            Hz = np.cos(np.pi*self.element.x/ self.params['a']) * C_10
-            self.E = np.stack((np.zeros_like(Ey), Ey, np.zeros_like(Ey)), axis=1)
-            self.H = np.stack((Hx, np.zeros_like(Hx), Hz), axis=1)          
+            indices = np.arange(self.params['N'])
+            E, H = self.cascade(indices, tuning_state)
+            self.E = E
+            self.H = H
+        elif feed_position == 'right':
+            indices = np.flip(np.arange(self.params['N']))
+            E, H = self.cascade(indices, tuning_state)
+            self.E = np.flip(E, axis=0)
+            self.H = np.flip(H, axis=0)
+        elif feed_position == 'both':
+            indices = np.arange(self.params['N'])
+            E_left, H_left = self.cascade(indices, tuning_state)
+            indices = np.flip(np.arange(self.params['N']))
+            E_right, H_right = self.cascade(indices, tuning_state)
+            self.E = E_left + np.flip(E_right, axis=0)
+            self.H = H_left + np.flip(H_right, axis=0)
+        elif feed_position == 'center':
+            indices = np.arange(self.params['N']//2, self.params['N'])
+            E_right, H_right = self.cascade(indices, tuning_state)
+            indices = np.flip(np.arange(self.params['N']//2))
+            E_left, H_left = self.cascade(indices, tuning_state)
+            self.E = np.concatenate((np.flip(E_left, axis=0), E_right), axis=0)
+            self.H = np.concatenate((np.flip(H_left, axis=0), H_right), axis=0)
 
     def compute_dipoles(self, tuning_state):
         self.m_e = self.element.alpha_e[tuning_state,:,:] @ self.E[:,:,None]
